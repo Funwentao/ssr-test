1e76e92120c5ad4b4328b715bf8f559db34b04f8
fun <wentao.fang@zhenai.com>
2019-03-07 19:41:05 +0800

    add config

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,31 @@
 <template>
-  <router-view></router-view>
+  <!--<router-view></router-view>-->
+  <div>
+    <about></about>
+    <home></home>
+  </div>
 </template>
+
+<script>
+    import about from './views/About.vue'
+    import home from './views/Home.vue'
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                msg: 'home'
+            }
+        },
+        components: {
+            about,
+            home
+        }
+    }
+</script>
+
+<style scoped lang="less">
+  .home-content {
+    color: red;
+  }
+</style>
